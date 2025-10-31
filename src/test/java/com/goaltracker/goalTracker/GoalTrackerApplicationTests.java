3fd59ab8959baf4b23f9b04d0488fc2d65aa1cf9
Pallavi Kumari <paalavikr028@gmail.com>
2025-10-31 11:10:28 +0530

    first commit

---
+++ src/test/java/com/goaltracker/goalTracker/GoalTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goaltracker.goalTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoalTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
